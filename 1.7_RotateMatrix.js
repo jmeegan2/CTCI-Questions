5627b97d8634e6be6de4f3a1eb0584e3b8fbdbb2
James Meegan <jmeegan2@owlmail.harford.edu>
2022-01-28 17:44:19 -0500

    not quite done 1.7 CtCi

---
+++ 1.7_RotateMatrix.js
@@ -0,0 +1,12 @@
+/*
+Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, 
+write a method to rotate the image by 90 degrees. Can you do this in place?
+
+I: NxN matrix
+O: rotated matrix - 90 degrees, clockwise
+C: rotate matrix in place, optimize 
+E: empty matrix, even and odd values for n
+
+time complexity:
+space complexity: 
+*/

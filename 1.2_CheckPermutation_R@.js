e15ee84dbf913261ffea588ce98aa9e9171a62c1
James Meegan <jmeegan2@owlmail.harford.edu>
2022-02-11 18:52:22 -0500

    done 1.2

--- 1.2_CheckPermutation_R@.js
+++ 1.2_CheckPermutation_R@.js
@@ -9,9 +9,19 @@ to join all elements of an array into a string.
 let checkPermu = (s1, s2) => {
     //if statement that will determine right off that bat that if they are the wrong lengths then they cannot be permutations
     //of eachother
-    if( s1.length !== s2.length) {
+    if (s1.length !== s2.length) {
         return false;
+    } // else sort and compares
+    //doesnt matter how its sorted as long as its sorted the same way
+    else {
+        var sortS1 = s1.split('').sort().join('');
+        var sortS2 = s2.split('').sort().join('')
+        return sortS1 === sortS2;
     }
-
     //declare variables that sort string somehow
+
 }
+
+console.log(checkPermu('ava', 'vaa'), true);
+
+console.log(checkPermu('aba', 'aaba'), false)

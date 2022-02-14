bff2fdd7a3ed9772a55a20faaca70cab09e3fd92
James Meegan <jmeegan2@owlmail.harford.edu>
2022-02-13 21:23:41 -0500

    folder creation for organization

---
+++ Ch. 2/2.1 Remove Dups.js
@@ -0,0 +1,16 @@
+/* 
+Remove Dups! Write code to remove duplicates from an unsorted linked list.
+FOLLOW UP
+How would you solve this problem if a temporary buffer is not allowed?
+*/
+
+"use strict";
+
+const users = [
+    {id: 32, fName: 'Mary', lName: 'Smith'},
+    {id: 65, fName: 'Steve', lName: 'Hancock'},
+    {id: 105, fName: 'LJ', lName: 'Estoque'},
+    {id: 65, fName: 'Steve', lName: 'Hancock'},
+    {id: 12, fName: 'Lynette', lName: 'Turner'},
+    {id: 32, fName: 'Mary', lName: 'Smith'},
+]
